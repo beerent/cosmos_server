a3e55ec094c5c0e76146de6a87e28c9da0fd7887
beerent <brentryczak@gmail.com>
2018-11-07 06:08:06 -0600

    adding challenge_attempts db table

--- web/database/current_db_update.sql
+++ web/database/current_db_update.sql
@@ -1,7 +1,10 @@
-ALTER TABLE `cosmos`.`challenge_attempts` DROP FOREIGN KEY `challenge_user_id_fk` ;
-ALTER TABLE `cosmos`.`challenge_attempts` CHANGE COLUMN `user_id` `user_id` INT(11) NOT NULL  , 
-  ADD CONSTRAINT `challenge_user_id_fk`
-  FOREIGN KEY (`user_id` )
-  REFERENCES `cosmos`.`users` (`id` )
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
+CREATE  TABLE `cosmos`.`challenge_attempts` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `user_id` INT NOT NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `challenge_attempts_user_id_fk_idx` (`user_id` ASC) ,
+  CONSTRAINT `challenge_attempts_user_id_fk`
+    FOREIGN KEY (`user_id` )
+    REFERENCES `cosmos`.`users` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);

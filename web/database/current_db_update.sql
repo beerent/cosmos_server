388fd6db09c01de1f6db69fe68d06334f6f05bd0
beerent <brentryczak@gmail.com>
2018-11-07 06:05:28 -0600

    updating database

--- web/database/current_db_update.sql
+++ web/database/current_db_update.sql
@@ -1,13 +1,7 @@
-drop table `cosmos`.`user_answers`;
-
-CREATE  TABLE `cosmos`.`users` (
-  `id` INT NOT NULL AUTO_INCREMENT ,
-  `username` VARCHAR(20) NOT NULL ,
-  `email` VARCHAR(45) NOT NULL ,
-  `password_salt` VARCHAR(45) NOT NULL ,
-  PRIMARY KEY (`id`) ,
-  UNIQUE INDEX `email_UNIQUE` (`email` ASC) ,
-  UNIQUE INDEX `username_UNIQUE` (`username` ASC) ,
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) );
-
-insert into users (username, email, password_salt) values ("beerent", "brentryczak@gmail.com", "turtle12");
+ALTER TABLE `cosmos`.`challenge_attempts` DROP FOREIGN KEY `challenge_user_id_fk` ;
+ALTER TABLE `cosmos`.`challenge_attempts` CHANGE COLUMN `user_id` `user_id` INT(11) NOT NULL  , 
+  ADD CONSTRAINT `challenge_user_id_fk`
+  FOREIGN KEY (`user_id` )
+  REFERENCES `cosmos`.`users` (`id` )
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;

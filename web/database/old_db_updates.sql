b76a30ec7a700192d51c4b4d8b1b90a6896f4525
beerent <brentryczak@gmail.com>
2018-11-11 17:07:40 -0600

    adding database updates

--- web/database/old_db_updates.sql
+++ web/database/old_db_updates.sql
@@ -52,3 +52,14 @@ CREATE  TABLE `cosmos`.`users` (
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) );
 
 insert into users (username, email, password_salt) values ("beerent", "brentryczak@gmail.com", "turtle12");
+
+  CREATE  TABLE `cosmos`.`challenge_attempts` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `user_id` INT NOT NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `challenge_attempts_user_id_fk_idx` (`user_id` ASC) ,
+  CONSTRAINT `challenge_attempts_user_id_fk`
+    FOREIGN KEY (`user_id` )
+    REFERENCES `cosmos`.`users` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);

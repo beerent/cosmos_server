25e85d65f6e8e4d58353f323aac8bd83b56eaa3a
beerent <brentryczak@gmail.com>
2019-01-10 04:38:06 -0600

    removing redunent column

--- web/database/current_db_update.sql
+++ web/database/current_db_update.sql
@@ -1,1 +1,5 @@
 ALTER TABLE `cosmos`.`stages` DROP INDEX `order_UNIQUE` ;
+
+ALTER TABLE `cosmos`.`challenge_answers` DROP FOREIGN KEY `challenge_answers_user_id_fk` ;
+ALTER TABLE `cosmos`.`challenge_answers` DROP COLUMN `user_id` 
+, DROP INDEX `challenge_answers_user_id_fk_idx` ;

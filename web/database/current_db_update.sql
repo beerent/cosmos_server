e36f8db7a4b2dd7d9b0c200ddc30eb9834df4f6e
beerent <brentryczak@gmail.com>
2019-01-24 16:17:10 -0600

    added kamil

--- web/database/current_db_update.sql
+++ web/database/current_db_update.sql
@@ -1,1 +1,2 @@
 insert into users (username, email, password_salt) values ("cosmic_bob", "nbarletta5@gmail.com", "brentsmells");
+insert into users (username, email, password_salt) values ("meatbadnoeat", "idk@gmail.com", "wheatbread");

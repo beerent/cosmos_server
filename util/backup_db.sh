448f95243e6aabe67583750eb9b71d180e45f04d
Ubuntu <ubuntu@ip-172-31-20-81.us-west-2.compute.internal>
2019-02-05 11:42:10 +0000

    updating backup database script

--- util/backup_db.sh
+++ util/backup_db.sh
@@ -1,3 +1,3 @@
 date=`date +%d_%m_%y_%H_%M`
-cmd="mysqldump -u root -pRyczak13! cosmos > ~/db_backup/cosmos_db_backup_$date.sql"
+cmd="mysqldump --no-create-info --insert-ignore -u root -pRyczak13! cosmos > ~/db_backup/cosmos_db_backup_$date.sql"
 eval $cmd

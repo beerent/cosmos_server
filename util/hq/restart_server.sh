eca07013cfd19a73bf21283267fea520b7a87051
Brent Ryczak <brentryczak@gmail.com>
2019-05-01 00:28:32 +0000

    cleaned up the start stop restart server scripts

--- util/hq/restart_server.sh
+++ util/hq/restart_server.sh
@@ -1,10 +1,4 @@
-COSMOS="/home/ubuntu/server/cosmos_ios/server/src/"
-
-pushd "$COSMOS"
-git pull
-sudo kill -9 $(ps aux | grep 'cosmos_server.js' | awk '{print $2}')
-sudo node cosmos_server.js live & disown
-popd
-
+./stop_server.sh
+./start_server.sh
 
 echo "server restarted!"

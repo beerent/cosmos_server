5ad53c5daf4eee11cc3b66dd007a11a8b62c079b
Ubuntu <ubuntu@ip-172-31-0-168.us-west-2.compute.internal>
2020-09-19 06:39:09 +0000

    [server] updated database

--- web/database/old_db_updates.sql
+++ web/database/old_db_updates.sql
@@ -293,3 +293,12 @@ DROP INDEX `username_UNIQUE` ;
 ;
 
 INSERT INTO `cosmos`.`users` (`username`, `email`, `password_salt`, `access_level`) VALUES ('beerent', 'guest', 'guest', '3');
+create table health
+(
+	id int auto_increment,
+	health_string varchar(200) not null,
+	constraint health_pk
+		primary key (id)
+);
+
+insert into health (health_string) values ('gaga X ari');

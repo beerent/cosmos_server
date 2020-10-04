457515eeba1a8bf450b23979fe1e05bda16d488b
Ubuntu <ubuntu@ip-172-31-12-15.us-west-2.compute.internal>
2020-10-04 03:10:38 +0000

    adding fully qualified paths

--- util/hq/restart_server.sh
+++ util/hq/restart_server.sh
@@ -1,4 +1,4 @@
-./stop_server.sh
-./start_server.sh
+/home/ubuntu/server/cosmos_server/util/hq/stop_server.sh
+/home/ubuntu/server/cosmos_server/util/hq/start_server.sh
 
 echo "server restarted!"
--- util/hq/restart_server_if_down.sh
+++ util/hq/restart_server_if_down.sh
@@ -3,6 +3,6 @@
 response=$(curl --write-out '%{http_code}' --silent --output /dev/null https://knowyourcosmos.com:13213/health)
 
 if [ $response -ne 200 ]; then
-  ./restart_mysql.sh
-  ./restart_server.sh
+  /home/ubuntu/server/cosmos_server/util/hq/restart_mysql.sh
+  /home/ubuntu/server/cosmos_server/util/hq/restart_server.sh
 fi

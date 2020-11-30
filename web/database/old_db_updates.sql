ecc115257f1f4d19c3cff59b13d5d732cc627b62
Ubuntu <ubuntu@ip-172-31-12-15.us-west-2.compute.internal>
2020-11-30 03:14:46 +0000

    [server] updated database

--- web/database/old_db_updates.sql
+++ web/database/old_db_updates.sql
@@ -302,3 +302,16 @@ create table health
 );
 
 insert into health (health_string) values ('gaga X ari');
+create table config
+(
+	id int auto_increment,
+	`key` varchar(50) not null,
+	value varchar(50) not null,
+	constraint config_pk
+		primary key (id)
+);
+
+create unique index config_key_uindex
+	on config (`key`);
+
+insert into config (`key`, `value`) values ("challenge_mode_timer_length", "10");

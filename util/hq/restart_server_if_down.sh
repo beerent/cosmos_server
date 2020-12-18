02d3fad251a102c3defc2ac0c82db351d1f3e52b
Ubuntu <ubuntu@ip-172-31-16-164.us-west-2.compute.internal>
2020-12-18 00:05:46 +0000

    added logging whenever server is down

--- util/hq/restart_server_if_down.sh
+++ util/hq/restart_server_if_down.sh
@@ -5,4 +5,7 @@ response=$(curl --write-out '%{http_code}' --silent --output /dev/null https://k
 if [ $response -ne 200 ]; then
   /home/ubuntu/server/cosmos_server/util/hq/restart_mysql.sh
   /home/ubuntu/server/cosmos_server/util/hq/restart_server.sh
+  
+  currentDate=`date`
+  echo "$currentDate" >> /home/ubuntu/server/cosmos_server/util/hq/server_down_log.txt
 fi

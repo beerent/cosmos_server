55e92acc9f2d9dfebc57ac8f0b472383904691d0
beerent <brentryczak@gmail.com>
2020-12-21 15:12:50 -0600

    cleaned up free memory output

--- util/remote/free_memory.sh
+++ util/remote/free_memory.sh
@@ -1,1 +1,2 @@
-ssh -i ~/.ssh/cosmos_final_key.pem ubuntu@www.knowyourcosmos.com './server/cosmos_server/util/hq/free_memory.sh'
+memory=`ssh -i ~/.ssh/cosmos_final_key.pem ubuntu@www.knowyourcosmos.com './server/cosmos_server/util/hq/free_memory.sh'`
+echo "cosmos memory: ${memory}MB"

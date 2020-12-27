600b5538baf60d811ca6f579b3ac720cde15c3d6
beerent <brentryczak@gmail.com>
2020-12-27 06:04:24 +0000

    fixed backup script

--- util/hq/backup_db.sh
+++ util/hq/backup_db.sh
@@ -1,5 +1,5 @@
 date=`date +%d_%m_%y_%H_%M`
 outfile="cosmos_db_backup_$date.sql"
-cmd="mysqldump --login-path=hq cosmos > ~/db_backup/$outfile"
+cmd="mysqldump --login-path=hq --no-tablespaces cosmos > ~/db_backup/$outfile"
 eval $cmd
 echo $outfile

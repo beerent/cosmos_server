13b6e1b3942260d68948c66bf43b5311c738f411
beerent <brentryczak@gmail.com>
2021-08-08 18:47:42 +0000

    [server] updated database

--- web/database/old_db_updates.sql
+++ web/database/old_db_updates.sql
@@ -409,3 +409,23 @@ create table cosmos_live_chat
 	constraint cosmos_live_chat_users_id_fk
 		foreign key (user_id) references users (id)
 );
+create table alerts
+(
+	id int,
+	`key` varchar(20) not null,
+	title varchar(100) not null,
+	alert varchar(1000) not null
+);
+
+create unique index alerts_id_uindex
+	on alerts (id);
+
+create unique index alerts_key_uindex
+	on alerts (`key`);
+
+alter table alerts
+	add constraint alerts_pk
+		primary key (id);
+
+alter table alerts modify id int auto_increment;
+
